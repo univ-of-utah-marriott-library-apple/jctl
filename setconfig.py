ce9055a94fffcb0a6dcbbd336f042427759cf7fc
Topher Nadauld <topher.nadauld@utah.edu>
2020-12-21 12:42:17 -0700

    Added version check for setconfig
    Since Setconfig is one of the first scripts that is ran it needs to check for the version as well.

--- setconfig.py
+++ setconfig.py
@@ -11,6 +11,9 @@ __copyright__ = 'Copyright (c) 2020 University of Utah, Marriott Library'
 __license__ = 'MIT'
 __version__ = "1.0.4"
 
+min_jamf_version = "0.4.7"
+
+
 import argparse
 import getpass
 import jamf
@@ -67,11 +70,23 @@ class Parser:
         """
         return self.parser.parse_args(argv)
 
+def check_version():
+    jamf_first, jamf_second, jamf_third = jamf.__version__.split(".")
+    min_first, min_second, min_third = min_jamf_version.split(".")
+
+    if ( int(jamf_first) <= int(min_first) and
+         int(jamf_second) <= int(min_second) and
+         int(jamf_third) < int(min_third)):
+         print(f"Your Version is: {jamf.__version__}, you need at least version {min_jamf_version} to run")
+         sys.exit()
+
 
 def main(argv):
     logger = logging.getLogger(__name__)
     args = Parser().parse(argv)
     logger.debug(f"args: {args!r}")
+
+    check_version()
 
     if args.test:
         api = jamf.API()
