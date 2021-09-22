33e82f7466aae09f62648ecb260c9f550b9511ca
Topher Nadauld <cnadauld@grailbio.com>
2021-09-22 16:53:07 -0600

    Updated version requirements

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setuptools.setup(
         "Development Status :: 4 - Beta",
     ],
     python_requires='>=3.6',
-    install_requires=['python-jamf'],
+    install_requires=['python-jamf>=0.6.6'],
 )
