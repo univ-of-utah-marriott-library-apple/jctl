1bd916ffac6f6f83d5a7eb49f572362dc0c3cf0e
Topher Nadauld <cnadauld@grailbio.com>
2021-09-22 15:59:48 -0600

    Structure change for PyPi
    To allow for installation and github actions, setup.py was added and files were move to better organize the installer.

---
+++ jctl/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+jctl
+Command line tool to use Python-Jamf
+"""
+
+__version__ = "1.1.4"
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jctl",
+    version="1.1.4",
+    author="The University of Utah",
+    author_email="mlib-its-mac@lists.utah.edu",
+    description="Command line tool that utilizes Python-Jamf",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/univ-of-utah-marriott-library-apple/jctl",
+    packages=setuptools.find_packages(),
+    scripts=['jctl/jctl','jctl/pkgctl'],
+    package_data={'': ['*.json']},
+    entry_points={
+        'console_scripts': ['jamfconfig=jamf.setconfig:main']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        # 4 - Beta
+        # 5 - Production/Stable
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires='>=3.6',
+    install_requires=['python-jamf'],
+)
